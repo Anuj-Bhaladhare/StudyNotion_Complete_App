ff25148aecf8f702073f85e481061b065b37e37c
Anuj Bhaladhare <anujbhaladhare@gmail.com>
2026-07-14 15:22:23 +0530

    initial setup is complete

--- Client_Side/src/App.js
+++ Client_Side/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        Hello StudyNotion by Anuj Developer
+        Hello StudyNotion 
+        <br />
+        by Anuj Developer
       </h1>
     </>
   );
